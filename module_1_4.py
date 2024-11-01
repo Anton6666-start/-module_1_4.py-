3315e2d0dbc6b1da17eb0869e65f8e15f1d47762
Anton6666-start <anton.shulga.01@gmail.com>
2024-11-01 22:44:48 +0800

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+#my_string = input("Здраствуйте, как Вас завут? ")
+#current_year = 2024
+#date_of_birth = input("Какого года Вы рождения? ")
+#age = current_year - int(date_of_birth)
+#print("Здраствуйте,", my_string)
+#print("В этом году Вам",age, "Лет")
+from os import replace
+print('Привет, я строка в верхнем регистри'.upper())
+print('Привет, я строка в нижнем регистри'.lower())
+print('Привет, я строка в нижнем регистри'.replace(' ','#'))
+print('Привет, я строка в нижнем регистри'.replace("Привет","Пока"))
+print('Привет, я строка в нижнем регистри'.replace("нижнем регистри","нижней строчке"))
